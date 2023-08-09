57d2dea1276a89aba237e3c460cfbc723194dfa3
Brenda Azeredo <135758864+brendafazeredo@users.noreply.github.com>
2023-08-09 10:57:42 -0300

    Update of some important stuff.

--- sketch.js
+++ sketch.js
@@ -1,35 +1,35 @@
-let person;
+let character;
 let grass;
+let enemy;
+let princess;
 
 let size = 64;
-
 let moveX = 0;
 let moveY = 0;
 
-let button;
-
 function setup() {
   createCanvas(576, 576);
-  person = loadImage('person.png');
+  character = loadImage('character.png');
   grass = loadImage('grass.png');
+  enemy = loadImage('enemy.png');
+  princess = loadImage('princess.png');
 }
 
 function draw() {
   background(220);
   
-  if (moveX < 0) {
+  if (moveX < 0){
     moveX = 0;
   }
-  
-  if (moveY < 0) {
+  if (moveY < 0){
     moveY = 0;
   }
   
-  if (moveX > size * 8) {
+  if (moveX > size * 8){
     moveX = size * 8;
   }
   
-  if (moveY > size * 8) {
+  if (moveY > size * 8){
     moveY = size * 8;
   }
   
@@ -39,13 +39,34 @@ function draw() {
     }
   }
   
-  image(person, moveX, moveY, size, size);
+  image(enemy, 128, 192, size, size);
+  image(enemy, 384, 128, size, size);
+  image(enemy, 320, 384, size, size);
+  image(enemy, 64, 448, size, size);
+  image(enemy, 192, 64, size, size);
+  image(enemy, 448, 320, size, size);
+  image(character, moveX, moveY, size, size);
+  image(princess, 512, 512, 80, 80); 
   
   if (moveX === size * 8 && moveY === size * 8) {
     rect(160, 160, 256, 256);
     textSize(35);
-    text('VICTORY', 210, 300);
-    button = createButton('Restart');
+    text('VICTORY', 230, 300);
+    button = createButton('RESTART');
+    button.mousePressed(reset);
+    noLoop();
+  }
+  
+  if (moveX === 128 && moveY === 192 ||
+      moveX === 384 && moveY === 128 ||
+      moveX === 320 && moveY === 384 ||
+      moveX === 64 && moveY === 448 ||
+      moveX === 192 && moveY === 64 ||
+      moveX === 448 && moveY === 320) {
+    rect(160, 160, 256, 256);
+    textSize(35);
+    text('DEFEAT', 232, 300);
+    button = createButton('RESTART');
     button.mousePressed(reset);
     noLoop();
   }
@@ -59,18 +80,15 @@ function reset() {
 }
 
 function keyPressed() {
-  if (keyIsDown(DOWN_ARROW)){
-    moveY += 64;
-  }
-  
   if (keyIsDown(UP_ARROW)){
     moveY -= 64;
   }
-  
+  if (keyIsDown(DOWN_ARROW)){
+    moveY += 64;
+  }
   if (keyIsDown(LEFT_ARROW)){
     moveX -= 64;
   }
-  
   if (keyIsDown(RIGHT_ARROW)){
     moveX += 64;
   }
